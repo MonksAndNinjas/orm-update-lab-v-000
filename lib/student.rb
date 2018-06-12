43f49858391395cd10531eb625905ca4f7fa29e5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 21:17:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT MAX(id) FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT MAX(id) FROM students")
   end
 
 end
