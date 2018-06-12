769ffa153aed83e5cc585c0885e6d9cb53b56ef2
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 21:45:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,14 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name)
   end
 
   def update
